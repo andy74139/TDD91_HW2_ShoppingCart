081a2e7c24a24b9a6df4c8f450e80cf28e61da02
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-06-18 23:24:48 +0800

    Init & first test without implement production code

---
+++ ShoppingCart/PotterShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ShoppingCart
+{
+    public class PotterShoppingCart
+    {
+        public int GetPriceForPotterI(int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShoppingCartTests/HarryPotterShoppingCartTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace ShoppingCart.Tests
+{
+    [TestFixture]
+    public class HarryPotterShoppingCartTests
+    {
+        [TestCase(1, ExpectedResult = 100)]
+        public int GetPriceForPotterITest(int amount)
+        {
+            var target = new PotterShoppingCart();
+            return target.GetPriceForPotterI(amount);
+        }
+    }
+}
