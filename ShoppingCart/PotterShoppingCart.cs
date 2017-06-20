8d42785d23c8cd0a2f83127247e0a40240fb2a2b
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-06-21 07:26:20 +0800

    Refactor duplicated feature for buying Potter I & II

--- ShoppingCart/PotterShoppingCart.cs
+++ ShoppingCart/PotterShoppingCart.cs
@@ -7,19 +7,6 @@ namespace ShoppingCart
 {
     public class PotterShoppingCart
     {
-        public int GetPriceForPotterI(int amount)
-        {
-            return 100*amount;
-        }
-
-        public int GetPriceForPotterOneTwo(int amount1, int amount2)
-        {
-            var seriesAmount = Math.Min(amount1, amount2);
-            var oneBookAmount = Math.Max(amount1, amount2) - seriesAmount;
-
-            return ((int) (100*2*0.95m))*seriesAmount + 100*oneBookAmount;
-        }
-
         public int GetPriceForPotters(IEnumerable<int> bookAmounts)
         {
             if(bookAmounts.Count() != 5)
--- ShoppingCartTests/PotterShoppingCartTests.cs
+++ ShoppingCartTests/PotterShoppingCartTests.cs
@@ -6,23 +6,11 @@ namespace ShoppingCart.Tests
     [TestFixture]
     public class PotterShoppingCartTests
     {
-        [TestCase(1, ExpectedResult = 100)]
-        public int GetPriceForPotterITest(int amount)
-        {
-            var target = new PotterShoppingCart();
-            return target.GetPriceForPotterI(amount);
-        }
-
-        [TestCase(1, 1, ExpectedResult = 190)]
-        public int GetPriceForPotterOneTwoTest(int amount1, int amount2)
-        {
-            var target = new PotterShoppingCart();
-            return target.GetPriceForPotterOneTwo(amount1, amount2);
-        }
-
-        [TestCase(new[] {1, 1, 1, 0}, ExpectedResult = 270)]
-        [TestCase(new[] {1, 1, 1, 1}, ExpectedResult = 320)]
-        [TestCase(new[] {1, 1, 1, 1, 1}, ExpectedResult = 375)]
+        [TestCase(new[] {1, 0, 0, 0, 0}, ExpectedResult = 100, TestName = "GetPriceForPottersTest_BuyPotterOne_ShouldBe100")]
+        [TestCase(new[] {1, 1, 0, 0, 0}, ExpectedResult = 190, TestName = "GetPriceForPottersTest_BuyPotterOneTwo_ShouldBe190")]
+        [TestCase(new[] {1, 1, 1, 0, 0}, ExpectedResult = 270, TestName = "GetPriceForPottersTest_BuyPotterOneToThree_ShouldBe270")]
+        [TestCase(new[] {1, 1, 1, 1, 0}, ExpectedResult = 320, TestName = "GetPriceForPottersTest_BuyPotterOneToFour_ShouldBe320")]
+        [TestCase(new[] {1, 1, 1, 1, 1}, ExpectedResult = 375, TestName = "GetPriceForPottersTest_BuyPotterOneToFive_ShouldBe375")]
         public int GetPriceForPottersTest(IEnumerable<int> bookAmounts)
         {
             var target = new PotterShoppingCart();
