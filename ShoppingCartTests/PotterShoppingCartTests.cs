663bf11fd37c9525bd31f0e2001c3a7f13de0878
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-06-21 07:31:34 +0800

    Add test for buying Potter I,II series & I to III series (綠燈)

--- ShoppingCartTests/PotterShoppingCartTests.cs
+++ ShoppingCartTests/PotterShoppingCartTests.cs
@@ -12,6 +12,7 @@ namespace ShoppingCart.Tests
         [TestCase(new[] {1, 1, 1, 1, 0}, ExpectedResult = 320, TestName = "GetPriceForPottersTest_BuyPotterOneToFourSeries_ShouldBe320")]
         [TestCase(new[] {1, 1, 1, 1, 1}, ExpectedResult = 375, TestName = "GetPriceForPottersTest_BuyPotterOneToFiveSeries_ShouldBe375")]
         [TestCase(new[] {1, 1, 2, 0, 0}, ExpectedResult = 370, TestName = "GetPriceForPottersTest_BuyPotterOneAndOneToThreeSeries_ShouldBe370")]
+        [TestCase(new[] {1, 2, 2, 0, 0}, ExpectedResult = 460, TestName = "GetPriceForPottersTest_BuyPotterOneTwoSeriesAndOneToThreeSeries_ShouldBe460")]
         public int GetPriceForPottersTest(IEnumerable<int> bookAmounts)
         {
             var target = new PotterShoppingCart();
