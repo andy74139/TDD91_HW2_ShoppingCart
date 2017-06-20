f927efb3313ddbf4abb7556ab7684da7648fe2c8
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-06-21 07:29:59 +0800

    Add test for buying Potter I & I to III series (綠燈)

--- ShoppingCartTests/PotterShoppingCartTests.cs
+++ ShoppingCartTests/PotterShoppingCartTests.cs
@@ -7,10 +7,11 @@ namespace ShoppingCart.Tests
     public class PotterShoppingCartTests
     {
         [TestCase(new[] {1, 0, 0, 0, 0}, ExpectedResult = 100, TestName = "GetPriceForPottersTest_BuyPotterOne_ShouldBe100")]
-        [TestCase(new[] {1, 1, 0, 0, 0}, ExpectedResult = 190, TestName = "GetPriceForPottersTest_BuyPotterOneTwo_ShouldBe190")]
-        [TestCase(new[] {1, 1, 1, 0, 0}, ExpectedResult = 270, TestName = "GetPriceForPottersTest_BuyPotterOneToThree_ShouldBe270")]
-        [TestCase(new[] {1, 1, 1, 1, 0}, ExpectedResult = 320, TestName = "GetPriceForPottersTest_BuyPotterOneToFour_ShouldBe320")]
-        [TestCase(new[] {1, 1, 1, 1, 1}, ExpectedResult = 375, TestName = "GetPriceForPottersTest_BuyPotterOneToFive_ShouldBe375")]
+        [TestCase(new[] {1, 1, 0, 0, 0}, ExpectedResult = 190, TestName = "GetPriceForPottersTest_BuyPotterOneTwoSeries_ShouldBe190")]
+        [TestCase(new[] {1, 1, 1, 0, 0}, ExpectedResult = 270, TestName = "GetPriceForPottersTest_BuyPotterOneToThreeSeries_ShouldBe270")]
+        [TestCase(new[] {1, 1, 1, 1, 0}, ExpectedResult = 320, TestName = "GetPriceForPottersTest_BuyPotterOneToFourSeries_ShouldBe320")]
+        [TestCase(new[] {1, 1, 1, 1, 1}, ExpectedResult = 375, TestName = "GetPriceForPottersTest_BuyPotterOneToFiveSeries_ShouldBe375")]
+        [TestCase(new[] {1, 1, 2, 0, 0}, ExpectedResult = 370, TestName = "GetPriceForPottersTest_BuyPotterOneAndOneToThreeSeries_ShouldBe370")]
         public int GetPriceForPottersTest(IEnumerable<int> bookAmounts)
         {
             var target = new PotterShoppingCart();
