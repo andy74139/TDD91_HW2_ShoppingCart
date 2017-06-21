c354687371c37fcfde254daf1ef4e76ab8902030
Andy Tsai Peng Yen <andy.tsai@titansoft.com.sg>
2017-06-21 09:03:50 +0800

    Add comment for test

--- ShoppingCartTests/PotterShoppingCartTests.cs
+++ ShoppingCartTests/PotterShoppingCartTests.cs
@@ -32,6 +32,7 @@ namespace ShoppingCart.Tests
             };
         }
 
+        // 7 tests in examples
         [TestCase(new[] {1, 0, 0, 0, 0}, ExpectedResult = 100, TestName = "GetPriceForPottersTest_BuyPotterOne_ShouldBe100")]
         [TestCase(new[] {1, 1, 0, 0, 0}, ExpectedResult = 190, TestName = "GetPriceForPottersTest_BuyPotterTwoSeries_ShouldBe190")]
         [TestCase(new[] {1, 1, 1, 0, 0}, ExpectedResult = 270, TestName = "GetPriceForPottersTest_BuyPotterThreeSeries_ShouldBe270")]
@@ -39,6 +40,7 @@ namespace ShoppingCart.Tests
         [TestCase(new[] {1, 1, 1, 1, 1}, ExpectedResult = 375, TestName = "GetPriceForPottersTest_BuyPotterFiveSeries_ShouldBe375")]
         [TestCase(new[] {1, 1, 2, 0, 0}, ExpectedResult = 370, TestName = "GetPriceForPottersTest_BuyPotterOneAndThreeSeries_ShouldBe370")]
         [TestCase(new[] {1, 2, 2, 0, 0}, ExpectedResult = 460, TestName = "GetPriceForPottersTest_BuyPotterTwoSeriesAndThreeSeries_ShouldBe460")]
+        // Addition tests to verify
         [TestCase(new[] {2, 0, 1, 0, 2}, ExpectedResult = 460, TestName = "GetPriceForPottersTest_BuyPotterTwoSeriesAndThreeSeriesWithourOrder_ShouldBe460")]
         [TestCase(new[] {1, 2, 3, 4, 5}, ExpectedResult = 1255, TestName = "GetPriceForPottersTest_BuyPotterOneEachSeries_ShouldBe1255")]
         [TestCase(new[] {4, 3, 6, 1, 7}, ExpectedResult = 1765, TestName = "GetPriceForPottersTest_BuyPotterEachSeries_ShouldBe1765")]
